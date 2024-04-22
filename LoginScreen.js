d910f151a23be30434e4c8a18bfad00e75e6ea4b
muppanya <156278615+muppanya@users.noreply.github.com>
2024-04-21 23:37:38 -0400

    Create LoginScreen.js

---
+++ LoginScreen.js
@@ -0,0 +1,28 @@
+// src/screens/LoginScreen.js
+import React, { useState } from 'react';
+import { View, Text, TextInput, Button } from 'react-native';
+import firebase from '../firebaseConfig';
+
+function LoginScreen({ navigation }) {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleLogin = async () => {
+    try {
+      await firebase.auth().signInWithEmailAndPassword(email, password);
+      navigation.navigate('Dashboard');
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <View>
+      <TextInput placeholder="Email" value={email} onChangeText={setEmail} />
+      <TextInput placeholder="Password" value={password} onChangeText={setPassword} secureTextEntry />
+      <Button title="Login" onPress={handleLogin} />
+    </View>
+  );
+}
+
+export default LoginScreen;
